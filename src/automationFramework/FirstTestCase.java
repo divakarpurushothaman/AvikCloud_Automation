44bd2ee5952d8ca61a675e1f8b504258187a33f3
Divakar <dpurushothaman@unicosolution.com>
2019-04-05 21:22:09 +0530

    Update FirstTestCase.java

--- src/automationFramework/FirstTestCase.java
+++ src/automationFramework/FirstTestCase.java
@@ -9,9 +9,9 @@ public class FirstTestCase {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "\\ToolsQA\\AvikCloud_Automation\\Drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		String URL = "https://avikcloud.com/";
+		String URL = "http://demoqa.com/frames-and-windows/";
 		driver.get(URL);
-		driver.findElement(By.xpath("//*[@id=\"post-344\"]/div/div/div/div/section[2]/div[2]/div/div/div/div[2]/div/div/h2")).click();
+		driver.findElement(By.xpath("/html/body")).click();
 		driver.close();
 
 	}
